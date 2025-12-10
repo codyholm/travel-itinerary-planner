8ef6666bb6d6a9c6b0b7bc45276c23615568a129
Cody H <holmcody@yahoo.com>
2025-12-10 00:40:48 -0800

    Add repository query methods for frontend API calls

--- src/main/java/com/example/demo/dao/DivisionRepository.java
+++ src/main/java/com/example/demo/dao/DivisionRepository.java
@@ -2,7 +2,17 @@ package com.example.demo.dao;
 
 import com.example.demo.entities.Division;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
-public interface DivisionRepository extends JpaRepository<Division, Long> {}
+public interface DivisionRepository extends JpaRepository<Division, Long> {
+    
+    /**
+     * Find all divisions for a given country.
+     * Exposed as /api/divisions/search/findByCountryId?countryId={id}
+     */
+    List<Division> findByCountryId(@Param("countryId") Long countryId);
+}
--- src/main/java/com/example/demo/dao/ExcursionRepository.java
+++ src/main/java/com/example/demo/dao/ExcursionRepository.java
@@ -2,7 +2,17 @@ package com.example.demo.dao;
 
 import com.example.demo.entities.Excursion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
-public interface ExcursionRepository extends JpaRepository<Excursion, Long> {}
+public interface ExcursionRepository extends JpaRepository<Excursion, Long> {
+    
+    /**
+     * Find all excursions for a given vacation.
+     * Exposed as /api/excursions/search/findByVacationId?vacationId={id}
+     */
+    List<Excursion> findByVacationId(@Param("vacationId") Long vacationId);
+}
